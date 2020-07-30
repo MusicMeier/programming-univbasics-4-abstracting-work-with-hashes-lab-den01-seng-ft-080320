4f1fe2fe08a85ef97d8b97a35d23716eb25ba314
Music Meier <musicm987@gmail.com>
2020-07-30 06:05:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,11 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   {
-    :key => 
+    :key =>
+    :hash =>
   }
 end
-read_from_hash
+
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
